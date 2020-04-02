3c8fa4a07c3c5439b387e1a593f3d0725963b9d7
ChungHyeongLee <cndgud0814@gmail.com>
2020-04-02 18:14:50 +0900

    gitignore emdfhrS

---
+++ src/main/java/com/lch/book/springboot/web/PostsApiController.java
@@ -0,0 +1,49 @@
+package com.lch.book.springboot.web;
+
+import com.lch.book.springboot.service.posts.PostsService;
+import com.lch.book.springboot.web.dto.PostsListResponseDto;
+import com.lch.book.springboot.web.dto.PostsResponseDto;
+import com.lch.book.springboot.web.dto.PostsSaveRequestDto;
+import com.lch.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+//등록
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+//수정 조회
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+    //조회속도 개선
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
+    //delete
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+}
